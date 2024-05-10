b797efcc2780be3a8285e505bc9cc32d2b446daa
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-05-10 11:18:02 -0300

    Consertando a animação do menu

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var isSidebarToggled = 0;
 let sidebar = document.getElementById('sidebar');
 let nav = document.getElementById('main-nav');
 let decor = document.querySelector('.decorative-tag');
+let page = document.querySelector('.page-container');
 
 if (window.innerWidth > 768 && isSidebarToggled == 1) {
     toggleSidebar()
@@ -12,27 +13,25 @@ function toggleSidebar() {
     if (isSidebarToggled == 0) {
         sidebar.style.left = 0;
 
-        nav.style.width = '40vw';
-        nav.style.height = '100vh';
-        nav.style.transition = '.5s';
         nav.style.marginTop = window.scrollY + 'px';
+        decor.style.marginTop = window.scrollY + 'px';
 
         document.body.style.backdropFilter = 'brightness(60%)';
         document.body.style.overflowY = 'hidden';
 
-        decor.style.visibility = 'hidden';
+        page.addEventListener('click', toggleSidebar);
 
         isSidebarToggled = 1;
     } else {
         sidebar.style.left = '-50vw';
 
-        nav.style.width = '100vw';
-        nav.style.height = '69px';
         nav.style.marginTop = 0;
-        
-        document.body.style.backdropFilter = 'none'
-        
-        decor.style.visibility = 'visible';
+        decor.style.marginTop = 0;
+
+        document.body.style.backdropFilter = 'none';
+        document.body.style.overflowY = 'scroll';
+
+        page.removeEventListener('click', toggleSidebar);
 
         isSidebarToggled = 0;
     }
