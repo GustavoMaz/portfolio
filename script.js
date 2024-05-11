907e20cee065347905fd8f648ea2f764dca2df02
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-05-11 15:32:40 -0300

    Ajustes no menu sidebar e imagens dos projetos

--- script.js
+++ script.js
@@ -4,27 +4,32 @@ let sidebar = document.getElementById('sidebar');
 let nav = document.getElementById('main-nav');
 let decor = document.querySelector('.decorative-tag');
 let bodyContainer = document.getElementById('body-container');
+let navLinks = document.querySelectorAll('.nav-item a');
 
 function toggleSidebar() {
     if (isSidebarToggled == 0) {
         sidebar.style.left = 0;
-
+/*
         nav.style.marginTop = window.scrollY + 'px';
         decor.style.marginTop = window.scrollY + 'px';
-
-        document.body.style.backdropFilter = 'brightness(60%)';
+*/
+        bodyContainer.style.backdropFilter = 'brightness(60%)';
         document.body.style.overflowY = 'hidden';
 
         bodyContainer.addEventListener('click', toggleSidebar);
 
+        for (i = 0; i < navLinks.length; i++) {
+            navLinks[i].addEventListener('click', toggleSidebar);
+        }
+
         isSidebarToggled = 1;
     } else {
         sidebar.style.left = '-50vw';
-
+/*
         nav.style.marginTop = 0;
         decor.style.marginTop = 0;
-
-        document.body.style.backdropFilter = 'none';
+*/
+        bodyContainer.style.backdropFilter = 'none';
         document.body.style.overflowY = 'scroll';
 
         bodyContainer.removeEventListener('click', toggleSidebar);
