e8caa50bf9aa181607452537524c7aace5f81f91
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-05-12 14:05:35 -0300

    Ajustando a posição da tag decorativa

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ document.getElementById('hamburger-button').addEventListener('click', toggleSide
 var isSidebarToggled = 0;
 let sidebar = document.getElementById('sidebar');
 let nav = document.getElementById('main-nav');
-let decor = document.querySelector('.decorative-tag');
 let bodyContainer = document.getElementById('body-container');
 let navLinks = document.querySelectorAll('.nav-item a');
 
 function toggleSidebar() {
     if (isSidebarToggled == 0) {
         sidebar.style.left = 0;
-/*
-        nav.style.marginTop = window.scrollY + 'px';
-        decor.style.marginTop = window.scrollY + 'px';
-*/
+
         bodyContainer.style.backdropFilter = 'brightness(60%)';
         document.body.style.overflowY = 'hidden';
 
@@ -25,10 +21,7 @@ function toggleSidebar() {
         isSidebarToggled = 1;
     } else {
         sidebar.style.left = '-50vw';
-/*
-        nav.style.marginTop = 0;
-        decor.style.marginTop = 0;
-*/
+
         bodyContainer.style.backdropFilter = 'none';
         document.body.style.overflowY = 'scroll';
 
