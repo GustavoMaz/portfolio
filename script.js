af2f93e3cff2a55ef76e3534e1b8fd22979485cf
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-08-02 09:30:50 -0300

    Mudança na identação do código, passando a utilizar dois espaços ao invés de quatro; Favicon importado e implementado

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ let bodyContainer = document.getElementById('body-container');
 let navLinks = document.querySelectorAll('.nav-item a');
 
 function toggleSidebar() {
-    if (isSidebarToggled == 0) {
-        sidebar.style.left = 0;
+  if (isSidebarToggled == 0) {
+    sidebar.style.left = 0;
 
-        bodyContainer.style.backdropFilter = 'brightness(60%)';
-        document.body.style.overflowY = 'hidden';
+    bodyContainer.style.backdropFilter = 'brightness(60%)';
+    document.body.style.overflowY = 'hidden';
 
-        bodyContainer.addEventListener('click', toggleSidebar);
+    bodyContainer.addEventListener('click', toggleSidebar);
 
-        for (i = 0; i < navLinks.length; i++) {
-            navLinks[i].addEventListener('click', toggleSidebar);
-        }
-
-        isSidebarToggled = 1;
-    } else {
-        sidebar.style.left = '-50vw';
-
-        bodyContainer.style.backdropFilter = 'none';
-        document.body.style.overflowY = 'scroll';
-
-        bodyContainer.removeEventListener('click', toggleSidebar);
-
-        isSidebarToggled = 0;
+    for (i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', toggleSidebar);
     }
+
+    isSidebarToggled = 1;
+  } else {
+    sidebar.style.left = '-50vw';
+
+    bodyContainer.style.backdropFilter = 'none';
+    document.body.style.overflowY = 'scroll';
+
+    bodyContainer.removeEventListener('click', toggleSidebar);
+
+    isSidebarToggled = 0;
+  }
 }
